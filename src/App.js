84992456764d53c6ecdbb9dbf641d90cfd94932c
GLoGitH <Gert.Longin@ventigrate.be>
2018-04-23 11:19:44 +0200

    class - constructor - dynamize - bind

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () =>
   return (
     <div className="App">
       <HelloWorld name="Gert"/>
+      <p />
       <HelloWorld name="Laurence"/>
     </div>
   );
--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,12 +1,12 @@
-import React from 'react';
-//import React, { Component } from 'react';
+//import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './index.css';
 import './HelloWorld.css';
 //import './App.css';
 
 
-
+/*
 const HelloWorld = props => 
 {
 //  return (<div className="App"><font color='red'>Hello World!</font></div>)
@@ -20,5 +20,37 @@ const HelloWorld = props =>
     </div>
   );
 };
+*/
+
+class HelloWorld extends Component 
+{
+   constructor(props) 
+   {
+        super(props);
+        this.state = { greeting: 'Hello' };
+        
+        this.goDutch = this.goDutch.bind(this);
+    }
+
+   goDutch()
+   {
+       this.setState({ greeting: 'Hallookes'});
+   }
+
+  render() 
+  {
+    return  (
+        <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-intro">
+            <font color='red'>{this.state.greeting} <i>{this.props.name}</i> !</font>
+          </p>
+          <button onClick={this.goDutch}>Vervlaams!</button>
+        </div>
+      );
+    }
+
+}
 
 export default HelloWorld;
